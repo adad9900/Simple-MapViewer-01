0946163ebd7124ae697229058e9bc8e8d94fc029
D7703_16 <d7703_16@D7703-16ui-iMac.local>
2018-06-07 11:51:49 +0900

    menu change

--- Simple MapViewer 01/ViewController.swift
+++ Simple MapViewer 01/ViewController.swift
@@ -7,19 +7,36 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myMapView: MKMapView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+        //위도 경도 설정
+        let loc : CLLocationCoordinate2D = CLLocationCoordinate2D (latitude: 35.165999, longitude: 129.072543)
+        
+        let span = MKCoordinateSpan(latitudeDelta: 0.009, longitudeDelta: 0.009)
+        
+        let region = MKCoordinateRegion(center: loc, span: span)
+        
+        myMapView.setRegion(region, animated: true)
+        
+        let anno = MKPointAnnotation()
+        anno.coordinate = loc
+        anno.title = "DIT 동의과학대학교"
+        anno.subtitle = "내꿈이 실현되는곳"
+        myMapView.addAnnotation(anno)
+        
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+
 
 
 }
 
+}
