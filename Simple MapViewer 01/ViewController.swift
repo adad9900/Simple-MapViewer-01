5dc6ff01c0efd03e3c3bdff41ba7f456f25c6a9c
D7703_16 <d7703_16@D7703-16ui-iMac.local>
2018-06-22 13:31:10 +0900

    test

--- Simple MapViewer 01/ViewController.swift
+++ Simple MapViewer 01/ViewController.swift
@@ -1,10 +1,4 @@
-//
-//  ViewController.swift
-//  Simple MapViewer 01
-//
-//  Created by D7703_16 on 2018. 6. 7..
-//  Copyright © 2018년 201412402. All rights reserved.
-//
+
 
 import UIKit
 import MapKit
@@ -13,8 +7,8 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var myMapView: MKMapView!
     
-    var foodStoreNames = ["늘해랑","아딸","번개반점","왕짜장","토마토도시락","홍콩반점"]
-    var foodStoreAddress = ["부산시 진구 양정동","부산시 진구 양정동","부산시 진구 양정동","부산시 진구 양정동","부산시 진구 양정동","부산시 진구 양정동"]
+    var foodStoreNames = ["늘해랑"]
+    var foodStoreAddress = ["부산광역시 부산진구 양정1동 305-1"]
 
     var i = 0
     var annos  = [MKPointAnnotation]()
@@ -56,51 +50,3 @@ class ViewController: UIViewController {
             }
         }
 
-        
-        // Do any additional setup after loading the view, typically from a nib.
-        
-        // 35.167809, 129.070544
-        // 번개반점 : 부산광역시 부산진구 양정동 418-282
-        //        let location = CLLocationCoordinate2D(latitude: 35.167809, longitude: 129.070544)
-        //        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
-        //        let region = MKCoordinateRegion(center: location, span: span)
-        //        myMapView.setRegion(region, animated: true)
-        //        /////
-        //
-        //        let annotation = MKPointAnnotation()
-        //        annotation.coordinate = location
-        //        annotation.title = "반개반점"
-        //        annotation.subtitle = "TEL: 051-860-1234"
-        //        myMapView.addAnnotation(annotation)
-        
-        /////
-        // let addr = "부산광역시 부산진구 양정동 418-282"
-        // let geoCoder = CLGeocoder()
-        
-//        geoCoder.geocodeAddressString(addr) {
-//            (placemarks: [CLPlacemark]?, error: Error?) -> Void in
-//            if let error = error {
-//                print(error)
-//                return
-//            }
-//                if let placemarks = placemarks {
-//                    let placemark = placemarks[0]
-//
-//
-//                    let loc = placemark.location?.coordinate
-//                    let span = MKCoordinateSpan(latitudeDelta: 0.09, longitudeDelta: 0.05)
-//
-//                    //                let region = MKCoordinateRegionMake(loc!, span)
-//                    //                let region = MKCoordinateRegionMakeWithDistance(loc!, 0.05, 0.05)
-//                    let region = MKCoordinateRegion(center: loc!, span: span)
-//                    self.myMapView.setRegion(region, animated: true)
-//
-//                    // annoation
-//                    let annotation = MKPointAnnotation()
-//                    annotation.coordinate = loc!
-//                    annotation.title = addr
-//                    annotation.subtitle = "번개반점"
-//                    self.myMapView.addAnnotation(annotation)
-
-            }
-        }
